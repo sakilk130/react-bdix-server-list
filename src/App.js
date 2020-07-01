236506301b095ca4b277577388cab38fa1ee5d82
sakilk130 <sakilk130@gmail.com>
2020-07-01 17:10:56 +0600

    complete searchBox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import ServerList from "./ServerList";
 import { ftpList } from "./ftpList";
 import SearchBox from "./SearchBox";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>BDIX Server List</h1>
-      <SearchBox />
-      <ServerList ftpList={ftpList} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ftpList: ftpList,
+      searchfield: "",
+    };
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+  render() {
+    const filterServer = this.state.ftpList.filter((ftpList) => {
+      return ftpList.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>BDIX Server List</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <ServerList ftpList={filterServer} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
-import { ftpList } from "./ftpList";
-import ServerList from "./ServerList";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
